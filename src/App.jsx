cfe9c977ddc4f9dfb50499b9057102b54e61d642
Mehdi-ehsani <metti.work@gmail.com>
2024-08-31 14:50:04 +0330

    add font to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react"
 
 function App() {
-
+  const [searchText , setSearchText] = useState("");
   return (
     <>
-     <h1 className="text-indigo-600" >Hellow Word!</h1>
+     <div><input type="text" value={searchText} onChange={(e) =>  setSearchText(e.target.value)} /></div>
+     <h1 className="text-indigo-600" >سلام</h1>
     </>
   )
 }
