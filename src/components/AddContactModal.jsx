2ca6e26841b276991b360d1812c3050f1def57ad
Mehdi-ehsani <metti.work@gmail.com>
2024-08-31 21:57:06 +0330

    change addContact value codes

--- src/components/AddContactModal.jsx
+++ src/components/AddContactModal.jsx
@@ -4,17 +4,35 @@ import jobImg from "../assets/icons/job.png";
 import { useState } from "react";
 
 const AddContactModal = ({ isOpen, onClose, contacts, setContacts }) => {
-	const [name, setName] = useState("");
-	const [email, setEmail] = useState("");
-	const [job, setjob] = useState("");
-	const addHandler = () => { 
-		contacts.push({name , email , job})
-     	localStorage.setItem("contacts" , JSON.stringify(contacts))
-		setName("")
-		setEmail("")
-		setjob("")
-		onClose()
-	}
+
+    const [contact, setContact] = useState({
+		name: "",
+		email: "",
+		job: "",
+	});
+	const changeHandler = (event) => {
+		const name = event.target.name;
+		const value = event.target.value;
+		if (name === "name") {
+			setContact({ ...contact, name: value });
+		}
+		if (name === "email") {
+			setContact({ ...contact, email: value });
+		}
+		if (name === "job") {
+			setContact({ ...contact, job: value });
+		}
+	};
+	const addHandler = () => {
+		contacts.push(contact);
+		localStorage.setItem("contacts", JSON.stringify(contacts));
+		setContact({
+			name: "",
+			email: "",
+			job: "",
+		});
+		onClose();
+	};
 	return (
 		<div
 			dir="ltr"
@@ -29,39 +47,52 @@ const AddContactModal = ({ isOpen, onClose, contacts, setContacts }) => {
 			>
 				<h1 className="text-2xl font-bold text-center">Add Contact</h1>
 				<div className="flex w-full h-10 border-2 border-gray-200 py-1 px-2 gap-2 rounded-lg">
-					<img src={userImg} alt="icon" className="w-7"/>
+					<img src={userImg} alt="icon" className="w-7" />
 					<input
+						name="name"
 						className="w-full outline-none border-none placeholder:text-gray-400"
 						type="text"
 						placeholder="Name"
-						value={name}
-						onChange={(e) => setName(e.target.value)}
+						value={contact.name}
+						onChange={changeHandler}
 					/>
 				</div>
 				<div className="flex w-full h-10 border-2 border-gray-200 py-1 px-2 gap-2 rounded-lg">
-					<img src={emailImg} alt="icon" className="w-7 "/>
+					<img src={emailImg} alt="icon" className="w-7 " />
 					<input
+						name="email"
 						className="w-full outline-none border-none placeholder:text-gray-400"
 						type="text"
 						placeholder="Email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
+						value={contact.email}
+						onChange={changeHandler}
 					/>
 				</div>
 				<div className="flex w-full h-10 border-2 border-gray-200 py-1 px-2 gap-2 rounded-lg">
-					<img src={jobImg} alt="icon" className="w-7"/>
+					<img src={jobImg} alt="icon" className="w-7" />
 					<input
+						name="job"
 						className="w-full outline-none border-none placeholder:text-gray-400"
 						type="text"
 						placeholder="Job"
-						value={job}
-						onChange={(e) => setjob(e.target.value)}
+						value={contact.job}
+						onChange={changeHandler}
 					/>
 				</div>
-                <div className="w-full flex gap-4">
-                    <button onClick={onClose} className="w-full h-12 rounded-lg  text-xl text-red-600 border-2 border-red-600">Close</button>
-                    <button onClick={addHandler} className="w-full h-12 rounded-lg  text-xl bg-green-600 text-white ">Add</button>
-                </div>
+				<div className="w-full flex gap-4">
+					<button
+						onClick={onClose}
+						className="w-full h-12 rounded-lg  text-xl text-red-600 border-2 border-red-600"
+					>
+						Close
+					</button>
+					<button
+						onClick={addHandler}
+						className="w-full h-12 rounded-lg  text-xl bg-green-600 text-white "
+					>
+						Add
+					</button>
+				</div>
 			</div>
 		</div>
 	);
